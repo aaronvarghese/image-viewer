ff7a9650d46e51a19344a50625b9690f29c9dd75
aaronvarghese <aaron.varghese.fun@gmail.com>
2021-05-03 19:00:34 +0530

    Home Page created

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,18 +1,26 @@
-import React, { Component } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import React, {Component} from 'react'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Login from './login/Login'
+import Home from './home/Home'
+import Error404 from './error404/Error404'
 
 export default class extends Component {
 
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Login />
-          </Route>
-        </Switch>
-      </Router>
-    )
-  }
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route exact path="/">
+                        <Login/>
+                    </Route>
+                    <Route exact path="/home">
+                        <Home/>
+                    </Route>
+                    <Route>
+                        <Error404/>
+                    </Route>
+                </Switch>
+            </Router>
+        )
+    }
 }
